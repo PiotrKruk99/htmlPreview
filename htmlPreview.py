c636a1a39b0d7889c8f0fd36f5f4feaacb0ba2f3
PiotrKruk99 <86235717+PiotrKruk99@users.noreply.github.com>
2021-11-20 14:02:40 +0100

    Add files via upload

--- htmlPreview.py
+++ htmlPreview.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
-from os.path import isfile
-from os.path import abspath
-from os.path import getmtime
-from argparse import ArgumentParser
+from os.path import isfile, abspath, getmtime
+from argparse import ArgumentParser, ArgumentTypeError
 from selenium.webdriver import Firefox
 from time import sleep
+
+
+def toFloat(value):
+    value = value.replace(',', '.')
+
+    try:
+        value = float(value)
+    except ValueError:
+        raise ArgumentTypeError('argument is not a number')
+
+    if value < 0:
+        raise ArgumentTypeError('argument must be not negative number')
+    return value
+
 
 parser = ArgumentParser()
 parser.description = 'Script for opening in browser and autorefreshing html file on change.'
 parser.add_argument('name', help='html file to be open')
+#parser.add_argument('-r', '--rate', type=float, default=2.0, help='time between file state check in seconds')
+parser.add_argument('-r', '--rate', type=toFloat, default=2.0,
+                    help='time between file state checks in seconds, only float non negative numbers acceptable')
 
 args = parser.parse_args()
 
@@ -25,10 +40,11 @@ driver.get('file://' + path)
 lastModified = getmtime(args.name)
 
 while True:
-    sleep(2)
+    sleep(args.rate)
 
     try:
-        test = driver.window_handles # throw exception when browser page is closed and quit script
+        # throw exception when browser page is closed and quit script
+        test = driver.window_handles
     except BaseException as e:
         quit()
 
